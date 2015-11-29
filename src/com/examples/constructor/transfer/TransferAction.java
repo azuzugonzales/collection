6b3c83ddc892b8e968ca1bb66f092a56d27e64fe
azuzugonzales <slipknot_13@mail.ru>
2015-11-29 18:38:00 +0200

    new commit Tranfer Action

---
+++ src/com/examples/constructor/transfer/TransferAction.java
@@ -0,0 +1,28 @@
+package com.examples.constructor.transfer;
+
+import com.examples.constructor.transfer.bean.Account;
+
+public class TransferAction {
+    private double transactionAmount;
+    public TransferAction(double amount) {
+        if (amount > 0) {
+            this.transactionAmount = amount;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+    public boolean transferIntoAccount(Account from, Account to) {
+        double demand = from.getAmount() - transactionAmount;
+        if (demand >= 0) {
+            from.setAmount(demand);
+            to.addAmount(transactionAmount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public double getTransactionAmount() {
+        return transactionAmount;
+    }
+}
+
