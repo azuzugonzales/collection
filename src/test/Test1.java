b1c24000dc999dd0c1e4e7e253b6d4a1f98f07fe
azuzugonzales <slipknot_13@mail.ru>
2016-03-24 11:07:56 +0200

    JSPs3

---
+++ src/test/Test1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("test CL");
+		Date today = new Date();
+        System.out.println(today);
+	}	
+}
